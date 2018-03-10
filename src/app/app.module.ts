044f0fcafccdad101a1cbc3572be804d68bd8422
Kuldeep Kamboj <kuldeepk1979@gmail.com>
2018-03-10 21:10:40 +0530

    move code to material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './/app-routing.module';
-import { MatSidenavModule, MatPaginatorModule, MatSortModule  } from '@angular/material';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatTableModule } from '@angular/material/table';
+import { MaterialModule } from './material/material.module';
+import { MaterialRoutingModule } from './material/material-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './login/login.component';
-import { UsersComponent } from './users/users.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    UsersComponent
   ],
   imports: [
     BrowserModule,
@@ -25,17 +17,11 @@ import { UsersComponent } from './users/users.component';
     ReactiveFormsModule,
     HttpModule,
     HttpClientModule,
-    AppRoutingModule,
+    MaterialModule,
+    MaterialRoutingModule,
     BrowserAnimationsModule,
-    MatSidenavModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule
   ],
-  exports: [MatSidenavModule, MatButtonModule, MatTableModule, MatPaginatorModule, MatSortModule],
+  exports: [],
   providers: [],
   bootstrap: [AppComponent]
 })
