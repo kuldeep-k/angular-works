cd7a31a471273c4e0c80fe65c18b7d48107fa8ba
kuldeep <kuldeep@ardigitech.com>
2018-03-10 21:10:40 +0530

    move code to material module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { UsersComponent } from './users/users.component';
+import { LoginComponent } from './material/login/login.component';
+import { UsersComponent } from './material/users/users.component';
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'users', component: UsersComponent }
