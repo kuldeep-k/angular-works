7ea5657c1d6f7da22fcc11c40183d795dde06ff5
Kuldeep Kamboj <kuldeepk1979@gmail.com>
2018-04-03 11:52:17 +0530

    added jwt token integration

--- src/app/material/login/login.component.ts
+++ src/app/material/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { LoginService } from './../../lib/login.service';
+import { Router } from '@angular/router'
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -9,10 +10,10 @@ import { LoginService } from './../../lib/login.service';
 })
 export class LoginComponent implements OnInit {
   public loginForm: any;
-
+  public errormsg: string;
   //  public fb: FormBuilder;
 
-  constructor(private fb: FormBuilder, private loginService: LoginService) { }
+  constructor(private fb: FormBuilder, private loginService: LoginService, private router: Router) { }
 
   ngOnInit() {
     this.loginForm = this.fb.group({
@@ -24,7 +25,21 @@ export class LoginComponent implements OnInit {
   login(form) {
     if (this.loginForm.dirty && this.loginForm.valid) {
       // alert(`Email: ${this.loginForm.value.email} Password: ${this.loginForm.value.password}`);
-      this.loginService.authenticate(this.loginForm.value.email, this.loginForm.value.password);
+      this.loginService.authenticate(this.loginForm.value.email, this.loginForm.value.password, (err, resp) => {
+        if (err !== false) {
+          if (err.status === 401) {
+            this.loginForm.controls['email'].setErrors({'incorrect': true});
+            this.errormsg = err.error.msg;
+            // console.error('Login Failed');
+            // console.error(err.error.msg);
+          }
+          // console.error(err)
+        } else {
+          // console.log(resp.token);
+          sessionStorage.setItem('jwtToken', resp.token);
+          this.router.navigate(['users']);
+        }
+      });
     }
   }
 
